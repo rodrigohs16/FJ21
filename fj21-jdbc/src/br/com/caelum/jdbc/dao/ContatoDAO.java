df42014f2f0e9be52d8bdd5f944ede48990e8baa
rodrigohs16 <rodrigohs16@gmail.com>
2018-03-24 14:55:07 -0300

    Atualiza novamente projeto

---
+++ fj21-jdbc/src/br/com/caelum/jdbc/dao/ContatoDAO.java
@@ -0,0 +1,46 @@
+package br.com.caelum.jdbc.dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.caelum.jdbc.ConnectionFactory;
+import br.com.caelum.model.Contato;
+
+public class ContatoDAO {
+
+	private Connection connection;
+
+	public ContatoDAO() {
+		this.connection = new ConnectionFactory().getConnection();
+	}
+
+	public void adiciona(Contato contato) {
+		String sql = "insert into contatos " + "(nome,email,endereco,dataNascimento)" + " values (?,?,?,?)";
+
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, contato.getNome());
+			stmt.setString(2, contato.getEmail());
+			stmt.setString(3, contato.getEndereco());
+			stmt.setDate(4, new Date(contato.getDataNascimento().getTimeInMillis()));
+
+			stmt.execute();
+			stmt.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException();
+		}		
+
+	}
+	
+	public void close(){
+		try {
+			this.connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
